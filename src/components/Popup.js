54316de7978960b00814851b3d56d067d42861fa
github username <github email address>
2020-08-08 01:42:02 -0400

    centering test

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -12,10 +12,12 @@ class Popup extends React.Component {
     return (
       <div className="popup">
         <p className="x" onClick={this.handleClickClose}>x</p>
+        <div className="inner">
         <h3>AUDIO DEMOS</h3>
         <p>Check out the audio demos of podcasts and music recorded!</p>
         <br/>
         <Link to="/audio"><button className='button' onClick={this.props.togglePopup}>Click here!</button></Link>
+        </div>
       </div>
     )
   }
