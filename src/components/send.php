9bb29011bc20ead6586abec2aba8a989942e6cef
github username <github email address>
2020-08-24 18:44:59 -0400

    extra file removed

--- src/components/send.php
+++
@@ -1,8 +0,0 @@
-You typed in: <?php print $_POST['myEmail'];?>
-
-<?php
-	$to = "eselbeus@gmail.com";
-	$subject = "Email from my website";
-	$body = "the user typed in ..." . $_POST['myEmail'];
-	mail($to, $subject, $body);
-?>
